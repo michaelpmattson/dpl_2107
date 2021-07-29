93bc73de0ab4ecbbba3f65d5dd56f62910f2e68b
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-29 14:36:03 -0600

    Refactor author

--- lib/author.rb
+++ lib/author.rb
@@ -12,24 +12,28 @@ class Author
   end
 
   def write(title, publication_date)
-    book = Book.new({title: title, author_first_name: @author_first_name, author_last_name: @author_last_name, publication_date: publication_date})
+    params = {
+      title: title,
+      author_first_name: @author_first_name,
+      author_last_name: @author_last_name,
+      publication_date: publication_date
+    }
+    book = Book.new(params)
     @books << book
     book
   end
 
   def first_book_year
-    first = @books.first.publication_year
-    @books.each do |book|
-      first = book.publication_year if book.publication_year.to_i < first.to_i
+    min = @books.min_by do |book|
+      book.publication_year
     end
-    first
+    min.publication_year
   end
 
   def last_book_year
-    last = @books.first.publication_year
-    @books.each do |book|
-      last = book.publication_year if book.publication_year.to_i > last.to_i
+    max = @books.max_by do |book|
+      book.publication_year
     end
-    last
+    max.publication_year
   end
 end
